5434deced91a408494f774a25902a6192efcaf55
chou-dai <c.daiki0916@gmail.com>
2023-03-27 21:17:43 +0900

    chage ci timing

--- hello_world/app.py
+++ hello_world/app.py
@@ -3,7 +3,7 @@ from typing import *
 
 
 def lambda_handler(event, context) -> Dict[str, Any]:
-    status: str = 200
+    status: int = 200
     message: str = "hello world"
     return {
         "statusCode": status,
