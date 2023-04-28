9f50a352fb1675b2f2a68784629d83f049c5ba36
chou-dai <c.daiki0916@gmail.com>
2023-04-29 00:06:46 +0900

    Install Style Library

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const Header: FC = () => {
+    return <header className="">aaa</header>;
+};
+
+export default Header;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,11 @@
-import React from "react";
-import "../styles/App.css";
+import React, { FC } from "react";
 
-function Home() {
+const Home: FC = () => {
     return (
         <div className="App">
             <header className="App-header">
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
+                <p className="text-red-500">
+                    Edit <code className=" text-yellow-400">src/App.tsx</code> and savfsdae to reload.
                 </p>
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
@@ -14,6 +13,6 @@ function Home() {
             </header>
         </div>
     );
-}
+};
 
 export default Home;
