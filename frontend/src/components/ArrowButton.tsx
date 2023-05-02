4fc93fc93968ed4ffa64c954b8f32cffb37cc5c4
chou-dai <c.daiki0916@gmail.com>
2023-05-03 01:36:14 +0900

    Change Style

--- frontend/src/components/ArrowButton.tsx
+++ frontend/src/components/ArrowButton.tsx
@@ -10,20 +10,20 @@ const ArrowButton = (props: Props) => {
     const { className, isDisable, onClick } = props;
 
     return (
-        <div className={`relative ${className} w-full flex items-center justify-center flex-col arrow-shadow`}>
+        <div className={`relative ${className} w-full flex items-center justify-center flex-col arrow-shadow sm:p-0 p-4 pt-8`}>
             <button
                 onClick={onClick}
                 disabled={isDisable}
                 className={`${
-                    isDisable && "opacity-60 cursor-not-allowed"
-                } arrow-outer flex items-center justify-center w-full aspect-square bg-blue-600`}
+                    isDisable && "opacity-40 cursor-not-allowed"
+                } arrow-outer flex items-center justify-center sm:w-full w-[40%] aspect-square bg-zinc-400 sm:rotate-0 rotate-90 sm:scale-100 scale-y-[2]`}
             >
                 <div
                     className={`${
-                        !isDisable && "hover:bg-blue-600"
-                    } arrow-inner flex items-center w-full aspect-square justify-center pr-5 bg-blue-500 transition-colors font-semibold text-white`}
+                        !isDisable && "hover:opacity-80"
+                    } arrow-inner flex items-center w-full aspect-square justify-center pr-5 bg-zinc-800 transition-opacity`}
                 >
-                    推 論
+                    <p className="text-white rotate-[-90deg] sm:rotate-0 sm:scale-100 scale-y-100 scale-50">推 論</p>
                 </div>
             </button>
         </div>
